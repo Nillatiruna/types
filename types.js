11d2288817a3d8ade0b6c36da692c37ce74ed32b
Nillatiruna <93864610+Nillatiruna@users.noreply.github.com>
2024-06-09 11:57:14 +0200

    Create types.js

---
+++ types.js
@@ -0,0 +1,6 @@
+//Auth variables
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
+export const LOAD_USER = 'LOAD_USER';
+export const ADD_LOADING = 'ADD_LOADING';
